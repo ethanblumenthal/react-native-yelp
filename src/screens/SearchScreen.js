e4971c29e62f91e650dc89884bd11e33d18130c3
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-12 10:49:55 -0500

    extracted logic into reusable hook and rendered api results to screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,17 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async searchTerm => {
-    try {
-      const res = await yelp.get('/search', {
-        params: {
-          term: searchTerm,
-          location: 'chicago',
-          limit: 50,
-        },
-      });
-      setResults(res.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong!');
-    }
+  const filterResultsByPrice = price => {
+    return results.filter(result => {
+      return result.price === price;
+    });
   };
 
   return (
@@ -28,6 +19,9 @@ const SearchScreen = () => {
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
